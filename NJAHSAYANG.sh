9f36da28bd4af69e28723553707f17fc98eed74c
QBZ <50372071+enablehacker01@users.noreply.github.com>
2019-05-24 16:09:05 +0700

    Add files via upload

---
+++ NJAHSAYANG.sh
@@ -0,0 +1,268 @@
+clear
+figlet NJAH-NDUT | lolcat
+echo "[+]─────────────────────────────────────────────────────[+]" | lolcat
+echo " | ••••••••••       |Indonesian Hacker Rulez|      ••••••••••••  |" | lolcat
+echo " | ───────────────────────────────────────────────────── |" | lolcat
+echo             
+echo " | Author      : Enablehacker                         |" | lolcat
+echo " | CodeName    : ./Tools Hacking                           |" | lolcat
+echo " | Instagram   : QBz => RBZ => KBZ                       |" | lolcat
+echo " | Facebook    : Njah Ndut             |" | lolcat
+echo " | Github      : https://github.com/Enablehacker           |" | lolcat
+echo " | Thanks To   : Enablehacker Indonesia => Malay                "| lolcat
+echo " | COPYRIGHT   : 2K19 Ehacker Tq                  |"  | lolcat
+echo "[+]─────────────────────────────────────────────────────[+]|"  |lolcat
+echo   "============================================================
+
+# Versi Installer 12.10
+# Nama : QBZ
+# Tim  : Enablehacker
+# Github : https://github.com/enablehacker01
+# Instagram : enablehacker
+
+============================================================="    | lolcat
+
+sleep 3
+
+echo "[00] Update & Upgrade Linux
+[01] Update & Upgrade Termux
+[02] Phising Sosial Media
+[03] Spam Sms
+[04] Member of ENABLEHACKER
+[05] Brute Force instagram
+[06] Install Tools
+[07] Nama User
+[08] The Choice
+[09] Spam Call
+[10] All Spam
+[11] py-rat
+[12] Enablemux
+[13] DDOS
+[14] FatRat
+[15] Cara Update Fat Rat
+[16] ah-myth
+[17] CCTV SCAN
+[18] exit"  | lolcat
+echo "[*] pilih nomor ya sayang : " | lolcat
+read nomor 
+
+if [ $nomor = 00  ] || [ $nomor]
+then
+clear
+apt-get update
+apt-get upgrade
+apt-get install lolcat
+apt-get install git
+apt-get install python
+apt-get install python2
+apt-get install php
+apt-get install figlet
+apt-get install mechanize
+apt-get install pip
+apt-get install curl
+apt-get install toilet
+apt-get install cowsay
+apt-get install python python2
+apt-get install openssh
+apt-get install ruby
+apt-get install tor
+apt-get install lolcat
+pip2 install --upgrade pip
+apt-get install --upgrade pip
+fi
+
+if [ $nomor = 01  ] || [ $nomor]
+then
+clear
+pkg update
+pkg upgrade
+pkg install lolcat
+pkg install git
+pkg install python
+pkg install python2
+pkg install php
+pkg install figlet
+pkg install mechanize
+pkg install pip
+pkg install curl
+pkg install toilet
+pkg install cowsay
+pkg install python python2
+pkg install openssh
+pkg install ruby
+pkg install tor
+pip install lolcat
+pip2 install lolcat
+pip2 install --upgrade pip
+fi
+
+if [ $nomor = 02  ] || [ $nomor]
+then
+clear
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $nomor = 03  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/spamsms.git
+cd spamsms
+bash spamRGR.sh
+fi
+
+if [ $nomor = 04  ] || [ $nomor]
+then
+clear
+echo "───────────────────────────────────────────" | lolcat
+echo "CEO" "AND" "FOUNDER" "Jaktim"  "[" "Enablehacler" "]" "Xploit"
+echo "───────────────────────────────────────────" | lolcat
+echo "CEO & FOUNDER" "Jaktim" "[" "Enablehacker" "]" "Xploit"
+echo "CODENAME  : KBZ"
+echo "SCHOOL    : ----"
+echo "REGION    : Jaktim"
+echo "───────────────────────────────────────────" | lolcat
+echo "LEADER" "Jaktim" "[" "Enablehacker" "]" "Xploit"
+echo "CODENAME  : RBZ"
+echo "SCHOOL    : ----"
+echo "REGION    : Jaktim"
+echo "───────────────────────────────────────────" |lolcat
+echo "CO-LEADER" "Jaktim" "[" "Enablehacker" "]" "Xploit"
+echo "CODENAME  : QBZ"
+echo "SCHOOL    : ----"
+echo "REGION    : Jaktim"
+echo "───────────────────────────────────────────" | lolcat
+fi
+
+if [ $nomor = 05  ] || [ $nomor]
+then
+clear
+apt-get install tor
+git clone https://github.com/enablehacker01/bfinstagram.git
+cd bfinstagram
+hmod +x install.sh 
+./install.sh
+chmod +x instainsane.sh
+bash instainsane.sh
+fi
+
+if [ $nomor = 06  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/toolsgila.git
+enablehacker01
+bangsataja01
+cd toolsgila
+bash install.sh
+fi
+
+if [ $nomor = 07  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/namauser.git
+enablehacker01
+bangsataja01
+cd namauser
+bash namauser.sh
+fi
+
+if [ $nomor = 08  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/thechoice.git
+enablehacker01
+bangsataja01
+cd thechoice
+bash thechoice.sh
+fi
+
+if [ $nomor = 09  ] || [ $nomor]
+then
+clear
+apt-get install python
+apt-get install python2
+git clone https://github.com/enablehacker01/spamcall.git
+enablehacker01
+bangsataja01
+cd spamcall
+python2 spamcall.py
+fi
+
+
+if [ $nomor = 10  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/allspam.git
+enablehacker01
+bangsataja01
+fi
+
+if [ $nomor = 11  ] || [ $nomor]
+then
+clear
+git clone https://github.com/thelinuxchoice/pyRAT 
+cd pyRAT 
+bash pyrat.sh
+fi
+
+if [ $nomor = 12  ] || [ $nomor]
+then
+clear
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux 
+python2 Lazymux.py
+fi
+
+if [ $nomor = 13  ] || [ $nomor]
+then
+clear
+git clone https://github.com/enablehacker01/DDOS.git
+enablehacker01
+bangsataja01
+cd DDOS
+Python hammer.py
+fi
+
+if [ $nomor = 14  ] || [ $nomor]
+then
+clear
+git clone https://github.com/Screetsec/TheFatRat.git
+cd TheFatRat
+chmod +x setup.sh && ./setup.shFatRat
+fi
+
+if [ $nomor = 15  ] || [ $nomor]
+then
+clear
+./update && chmod +x setup.sh && ./setup.sh
+git pull && chmod +x setup.sh && ./setup.sh
+fi
+
+if [ $nomor = 16  ] || [ $nomor]
+then
+clear
+git clone https://github.com/AhMyth/AhMyth-Android-RAT.git
+cd AhMyth-Android-RAT/AhMyth-Server
+npm start
+fi
+
+if [ $nomor = 17  ] || [ $nomor]
+then
+clear
+git clone https://github.com/zlucifer/all_seeing
+cd all_seeing
+chmod +x *
+termux-fix-shebang cctv.sh
+./cctv.sh
+fi
+
+if [ $nomor = 18  ] || [ $nomor]
+then
+clear
+echo "Terima kasih" "Njah Ndut" "Toolsnya Bagus" "[" "Makasih" "]" "Rafael"
+echo "CODENAME  : 31 - 12 - 16"
+echo "SCHOOL    : Udah Pinter Dari Lahir" 
+echo "REGION    : Kepo"
+echo "───────────────────────────────────────────" | lolcat
+fi 
